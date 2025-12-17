007c9d2417e92162a614f823baf947337651efb4
Max Reis <maxreis507@gmail.com>
2025-12-17 17:23:45 +0100

    feat: add registration form view

---
+++ app/Views/register/index.php
@@ -0,0 +1,22 @@
+<div class="login-form-wrapper">
+    <div class="slogan">
+        <h1>MEJIWOO</h1>
+        <h2>미지우</h2>
+    </div>
+
+    <div class="auth-message">
+        <h2>Create your account</h2>
+    </div>
+    
+    <form action="" method="post" class="login-form">
+        <div>
+            <input type="text" name="firstname" required placeholder="First Name">
+            <input type="text" name="lastname" required placeholder="Last Name">
+            <input type="text" name="email" required placeholder="Email Address">
+            <input type="text" name="password" required placeholder="Password">
+        </div>
+    
+        <button type="submit">Sign up</button>
+    </form>
+    <p>Already client? <a href="/login">Login</a> </p> 
+</div>
