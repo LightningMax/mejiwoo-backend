8bb5984dc376a39374a059fe2d6ff9c6ea0a5b48
Max Reis <maxreis507@gmail.com>
2025-12-17 17:23:38 +0100

    feat: add routes for user registration, login, logout, and dashboard

--- public/index.php
+++ public/index.php
@@ -9,8 +9,19 @@ use Mini\Core\Router;
 // Table des routes minimaliste
 $routes = [
     ['GET', '/', [Mini\Controllers\HomeController::class, 'index']],
+    
+    ['GET', '/register', [Mini\Controllers\RegisterController::class, 'index']],
+    ['POST', '/register', [Mini\Controllers\RegisterController::class, 'register']],
+    
+    ['GET', '/login', [Mini\Controllers\LoginController::class, 'index']],
+    ['POST', '/login', [Mini\Controllers\LoginController::class, 'login']],
+    ['GET', '/logout', [Mini\Controllers\LoginController::class, 'logout']],
+    
+    ['GET', '/dashboard', [Mini\Controllers\DashboardController::class, 'index']],
+    
     ['GET', '/products', [Mini\Controllers\ProductsController::class, 'index']],
     ['GET', '/products/product-detail', [Mini\Controllers\ProductsController::class, 'showProductDetail']],
+    
     ['GET', '/admin', [Mini\Controllers\AdminController::class, 'index']],
     ['POST', '/admin', [Mini\Controllers\AdminController::class, 'index']],
     ['POST', '/admin/add-product', [Mini\Controllers\AdminController::class, 'addProduct']],
