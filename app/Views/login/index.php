3cf9adc86ab99fccfc7539d21aa7efe4510df744
Max Reis <maxreis507@gmail.com>
2025-12-17 17:23:51 +0100

    feat: add login form view

---
+++ app/Views/login/index.php
@@ -0,0 +1,21 @@
+<div class="login-form-wrapper">
+    <div class="slogan">
+        <h1>MEJIWOO</h1>
+        <h2>미지우</h2>
+    </div>
+
+    <div class="auth-message">
+        <h2>Sign in to your account</h2>
+    </div>
+
+    <form action="/login" method="post" class="login-form">
+        <div>
+            <input type="text" name="email" required placeholder="Email Address">
+            <input type="text" name="password" required placeholder="Password">
+        </div>
+    
+        <button type="submit">Log in</button>
+    </form>
+
+    <p>Not a client? <a href="/register">Sign up</a> </p> 
+</div>
